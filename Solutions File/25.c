16e98a3572d9ae2f7183faeb75f568e0746e6ecd
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-07-16 13:10:05 +0530

    Update 25.c

--- Solutions File/25.c
+++ Solutions File/25.c
@@ -1,25 +1,59 @@
-#include <stdio.h>
+// Dalip's approach
+// #include <stdio.h>
 
-int main()
-{
+// int main()
+// {
 
-    int arr[10] = {2, 5, 3, 1, 4, 6, 0, 9, 8, 7};
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10 - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                char temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ",arr[i]);
-    }
+//     int arr[10] = {2, 5, 3, 1, 4, 6, 0, 9, 8, 7};
+//     for (int i = 0; i < 10; i++)
+//     {
+//         for (int j = 0; j < 10 - i - 1; j++)
+//         {
+//             if (arr[j] > arr[j + 1])
+//             {
+//                 char temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < 10; i++)
+//     {
+//         printf("%d ",arr[i]);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+// Dalip's approach
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i, j, length;
+//     int ar[] = {4, 1, 5, 2, 3};
+
+//     length = sizeof(ar) / sizeof(ar[0]);
+
+//     for (i = 0; i < length; i++)
+//     {
+//         for (j = i + 1; j < length; j++)
+//         {
+//             if (ar[i] > ar[j])
+//             {
+//                 ar[i] = ar[i] + ar[j];
+//                 ar[j] = ar[i] - ar[j];
+//                 ar[i] = ar[i] - ar[j];
+//             }
+//         }
+//     }
+
+//     // Print sorted array after sorting is done
+//     for (i = 0; i < length; i++)
+//     {
+//         printf(" %d", ar[i]);
+//     }
+
+//     return 0;
+// }
